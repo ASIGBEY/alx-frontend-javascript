76bbf575f5453140dc98e183ce063b2ecd871997
Asigbey Theophilus Tetteh <asigbey0242@gmail.com>
2024-09-30 11:13:22 -0700

    final

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;  // Outer variable
-  const task2 = true;  // Outer variable
+  const task = false;
+  const task2 = true;
 
+  /* eslint-disable no-unused-vars */
   if (trueOrFalse) {
-    const task = true;   // This is block-scoped and won't overwrite the outer 'task'
-    const task2 = false; // This is block-scoped and won't overwrite the outer 'task2'
+    const task = true;
+    const task2 = false;
   }
+  /* eslint-enable no-unused-vars */
 
-  return [task, task2];  // Outer task and task2 are returned
+  return [task, task2];
 }
