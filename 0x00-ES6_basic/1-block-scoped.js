6e6fe166f6167da43f242634a2bbb46e91277950
Asigbey Theophilus Tetteh <asigbey0242@gmail.com>
2024-09-30 11:09:46 -0700

    final

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;  // Outer variable
-  let task2 = true;  // Outer variable
+  const task = false;  // Outer variable
+  const task2 = true;  // Outer variable
 
   if (trueOrFalse) {
-    var task = true;   // This is block-scoped and won't overwrite the outer 'task'
-    var task2 = false; // This is block-scoped and won't overwrite the outer 'task2'
+    const task = true;   // This is block-scoped and won't overwrite the outer 'task'
+    const task2 = false; // This is block-scoped and won't overwrite the outer 'task2'
   }
 
   return [task, task2];  // Outer task and task2 are returned
